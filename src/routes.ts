9f910286c1e4358edaf9d5194684cefb1c1deb8d
ElitonSouzaDev <eliton@sisplansistemas.com.br>
2021-10-19 13:09:13 -0300

    fase 2

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controller/AuthenticateUserController';
+import { CreateMessageController } from './controller/CreateMessageController';
+import { ensureAuthenticated } from './middleware/ensureAuthenticate';
 
 
 const router = Router();
 router.post('/authenticate', new AuthenticateUserController().handle);
-
+router.post('/messages', ensureAuthenticated, new CreateMessageController().handle);
 export { router };
