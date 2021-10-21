30b550f119adae6cc2455cdb8b5ec0f5ce9ebaf9
ElitonSouzaDev <eliton@sisplansistemas.com.br>
2021-10-21 13:14:10 -0300

    first class

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import express from 'express';
 import "dotenv/config";
 import { router } from './routes';
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
 
 const app = express();
+app.use(cors());
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*'
+  }
+});
+
+io.on('connection', socket => {
+  console.log('Usuário conectado no socket '+socket.id);
+});
 app.use(express.json());
 app.use(router);
 
@@ -16,5 +30,5 @@ app.get('/signin/callback', (request, response)=> {
 })
 
 
-app.listen(4000, () => console.log('🐱‍🏍 Server is running on port 4000'));
 
+export { serverHttp, io};
---
+++ src/services/GetLast3MessagesService.ts
@@ -0,0 +1,10 @@
+import { io } from "../app";
+import prismaClient from "../prisma";
+
+class GetLast3MessagesService {
+  async execute(text: string, user_id: string) {
+    
+  }
+}
+
+export { GetLast3MessagesService };
